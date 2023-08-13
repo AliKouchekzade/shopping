64c3c27ee9325fb26d3079eb1bc7343789c70b93
Ali Kouchekzade <akmg1660@gmail.com>
2023-08-13 09:43:10 +0330

    layout

---
+++ src/layout/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="bg-teal-400 px-10 py-2">
+      <nav>
+        <ul className="flex gap-x-10">
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
